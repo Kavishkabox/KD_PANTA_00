c71dd40761927dfb6ed8641c4c8816bef9d85f05
KavishkaIroshanb <Kavishkairoshan@outlook.com>
2024-12-06 19:05:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
------------
 
-***WELCOME TO SILENT-SOBX-MD PLUGINS LIST CREATED BY SILENTLOVER432***
-
------------
-
-***THE WORLD BEST WHATSAPP BOT***
-
-----------
---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=WRAGCAwL#p8cnpSx29qQ8l--PHCFMhdKc8mNImx0ZAe7Wcr_Tlrg",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/fxhw0z8c.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "👿☠️_KD_PANTA_00_☠️👿",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
