1519d0aceb3ea2b3b48c7766e5fa1f1f23929d70
Kavishkabox <kdpantabot@outlook.com>
2024-12-08 19:26:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=mcNm3IIS#7hxKqpsuGiTTUo31YZuAITzIIY8IW1_7OW1UE_iGCpc",
+    SESSION_ID: process.env.SESSION_ID || "DNwAwDwC#f_Txn_E4qqbrnIWd9Dz_3q7z-Yg-mPmDVIhNNVEip3U",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
